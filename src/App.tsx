14abbfa42d635eb348468a5d7d5c730766c74d95
Robert-J-WANG <robin.nz420@gmail.com>
2023-12-23 20:24:02 +1300

    2-安装antd-mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "antd-mobile";
+
 function App() {
   return (
     <>
       <p>this is an app</p>
+      {/* 测试antd-mobile组件库安装成功 */}
+      <Button color="warning" fill="outline">
+        this is a button
+      </Button>
     </>
   );
 }
